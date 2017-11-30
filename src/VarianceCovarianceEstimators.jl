475a734e997af04710e3098eb46a5c4ab2a1ceca
Nosferican <naoyabpr@gmail.com>
2017-11-29 22:11:13 -0600

    Progress

--- src/VarianceCovarianceEstimators.jl
+++ src/VarianceCovarianceEstimators.jl
@@ -3,6 +3,7 @@ __precompile__(true)
 module VarianceCovarianceEstimators
 
 using StatsBase
+import StatsBase: vcov
 
 ## Abstract and Structs
 abstract type AbstractVCE end
@@ -19,46 +20,44 @@ VCESymHCMapper = Dict([(:OLS, OLS()),(:HC1, HC1()),(:HC2, HC2()),(:HC3, HC3())])
 VCESymCRMapper = Dict([(:HC1, CRVE1()), (:HC2, CRVE2()), (:HC3, CRVE3())])
 
 ## Methods packages should implement
-function bread(obj::StatsBase.RegressionModel)
-	mm = StatsBase.modelmatrix(obj)
+function bread(obj::RegressionModel)
+	mm = modelmatrix(obj)
 	output = inv(cholfact!(mm.'mm))
 	return output
 end
-clusters(obj::StatsBase.RegressionModel) = Vector{Vector{Int64}}()
+clusters(obj::RegressionModel) = Vector{Vector{Int64}}()
 
 ## API
-function StatsBase.vcov(model::StatsBase.RegressionModel, estimator::Symbol)
+function vcov(model::RegressionModel, estimator::Symbol)
     groups = clusters(model)
     if isempty(groups)
         @assert haskey(VCESymHCMapper, estimator) (@sprintf "%s is not a valid variance covariance estimator. Valid estimators are: OLS, HC1, HC2, HC3." estimator)
-        output = StatsBase.vcov(model, get(VCESymHCMapper, estimator, OLS()))
+        output = vcov(model, get(VCESymHCMapper, estimator, OLS()))
     else
         @assert haskey(VCESymCRMapper, estimator) (@sprintf "%s is not a valid cluster robust variance covariance estimator. Valid estimators are: HC1, HC2, HC3." estimator)
-        output = StatsBase.vcov(model, get(VCESymCRMapper, estimator, HC1()), groups)
+        output = vcov(model, get(VCESymCRMapper, estimator, HC1()), groups)
     end
     return output
 end
 
 ## Core Methods
-function StatsBase.vcov(model::StatsBase.RegressionModel, estimator::OLS)
-    output = StatsBase.deviance(model) * bread(model)
-end
-function StatsBase.vcov(model::StatsBase.RegressionModel, estimator::AbstractHCVCE)
-    mm = StatsBase.modelmatrix(model)
+vcov(model::RegressionModel, estimator::OLS) = deviance(model) * bread(model)
+function vcov(model::RegressionModel, estimator::AbstractHCVCE)
+    mm = modelmatrix(model)
     Bread = bread(model)
-    û = StatsBase.residuals(model)
+    û = residuals(model)
     output = Bread * meat(estimator, mm, û) * Bread
     if isa(estimator, HC1)
-        output .*= StatsBase.nobs(model) / StatsBase.dof_residual(model)
+        output .*= nobs(model) / dof_residual(model)
     end
     return output
 end
-function StatsBase.vcov(model::StatsBase.RegressionModel,
+function vcov(model::RegressionModel,
 						estimator::AbstractCRVCE,
 						Clusters::Vector{Vector{Vector{T}}}) where T <: Integer
-    mm = StatsBase.modelmatrix(model)
+    mm = modelmatrix(model)
     Bread = bread(model)
-    û = StatsBase.residuals(model)
+    û = residuals(model)
     G = BitMatrix(zeros(size(mm, 1), size(mm, 1)))
     for dimension ∈ Clusters
         for level ∈ dimension
@@ -85,23 +84,14 @@ function StatsBase.vcov(model::StatsBase.RegressionModel,
     output = Bread * mm.' * (û * û.' .* G) * mm * Bread
     # g = length(unique(map(col -> find(G[:,col]), 1:size(G, 2))))
 	g = minimum(length.(Clusters))
-    rdf = StatsBase.dof_residual(model)
-    output .*= g / (g - 1) * (StatsBase.nobs(model) - 1) / rdf
+    rdf = dof_residual(model)
+    output .*= g / (g - 1) * (nobs(model) - 1) / rdf
     return output, min(rdf, g - 1)
 end
 
 ## Helper
-function meat(estimator::HC1, X::AbstractMatrix, û::AbstractVector)
-    output = X.' * Diagonal(û.^2) * X
-    return output
-end
-function meat(estimator::HC2, X::AbstractMatrix, û::AbstractVector)
-    output = X.' * Diagonal(û.^2 ./ (1 .- diag(X * pinv(X)))) * X
-    return output
-end
-function meat(estimator::HC3, X::AbstractMatrix, û::AbstractVector)
-    output = X.' * Diagonal((û ./ (1 .- diag(X * pinv(X)))).^2) * X
-    return output
-end
+meat(estimator::HC1, X::AbstractMatrix, û::AbstractVector) = X.' * Diagonal(û.^2) * X
+meat(estimator::HC2, X::AbstractMatrix, û::AbstractVector) = X.' * Diagonal(û.^2 ./ (1 .- diag(X * pinv(X)))) * X
+meat(estimator::HC3, X::AbstractMatrix, û::AbstractVector) = X.' * Diagonal((û ./ (1 .- diag(X * pinv(X)))).^2) * X
 
 end
